e754f34f4d17659a3435cc4abe768d932fe360e6
demosthenesd <demosthenesd@icloud.com>
2023-06-14 16:10:21 +1000

    Started on the edit functionality for each of the products
    Displays all products to a table and button for each to edit

--- admin/pages/products/new.js
+++ admin/pages/products/new.js
@@ -1,16 +1,26 @@
 import Layout from "@/Components/Layout";
 import { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function NewProduct() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
+  const[goToProducts, setGoToProducts] = useState(false);
+
+  const router = useRouter();
 
   async function createProduct(ev) {
     ev.preventDefault();
     const data = { title, description, price };
     await axios.post("/api/products", data);
+    setGoToProducts(true);
+  }
+
+  if(goToProducts)
+  {
+     router.push('/products')
   }
 
   return (
