c0c2a464e967975e93f2a47676de4b840440ae2d
demosthenesd <demosthenesd@icloud.com>
2023-06-19 16:06:20 +1000

    Next step: Amazon S3 buckets 2:22:14
    stuck, my bank accnt for some reason is not verified

--- admin/Components/ProductForm.js
+++ admin/Components/ProductForm.js
@@ -8,6 +8,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -22,18 +23,30 @@ export default function ProductForm({
 
     if (_id) {
       //update
-      await axios.put('/api/products', {...data,_id});
-
+      await axios.put("/api/products", { ...data, _id });
     } else {
       //create
       await axios.post("/api/products", data);
     }
     setGoToProducts(true);
-
   }
 
   if (goToProducts) {
     router.push("/products");
+  }
+
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+      await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+    }
   }
 
   return (
@@ -45,6 +58,30 @@ export default function ProductForm({
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
+
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="cursor-pointer w-24 h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-600 rounded-xl bg-gray-100">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && <div>No images in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
