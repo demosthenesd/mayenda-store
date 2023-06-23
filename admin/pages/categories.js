06abe4c34b640d2247aca16ac5998273198af0f2
demosthenesd <demosthenesd@icloud.com>
2023-06-23 22:58:07 +1000

    3:47:35 || added the edit function on categories
    Can now add Parent category to each of the categories.
    
    Ex. Category: Iphone has a parent category of Mobile

--- admin/pages/categories.js
+++ admin/pages/categories.js
@@ -3,34 +3,73 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Categories() {
+  const [editedCategory, setEditedCategory] = useState(null);
   const [name, setName] = useState("");
-
+  const [parentCategory, setParentCategory] = useState("");
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
     axios.get("/api/categories").then((res) => {
       setCategories(res.data);
     });
-  }, []);
+  }
 
   async function saveCategory(ev) {
     ev.preventDefault();
-    await axios.post("/api/categories", { name });
+    const data = { name, parentCategory };
+
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+
     setName("");
+    fetchCategories();
   }
+
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+
+  function deleteCategory(category) {}
 
   return (
     <Layout>
       <h1>Categories</h1>
-      <label>New Category name</label>
+      <label>
+        {editedCategory
+          ? `Edit category: ${editedCategory.name}`
+          : "Create New Category"}{" "}
+      </label>
       <form onSubmit={saveCategory} className="flex gap-1">
         <input
-          className="mb-0 mt-3"
+          className="mb-0 "
           type="text"
           placeholder={"Category name"}
           onChange={(ev) => setName(ev.target.value)}
           value={name}
         />
+
+        <select
+          className="m-0 "
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value="none">No parent category</option>
+          {categories.length &&
+            categories.map((category) => (
+              <option value={category._id}>{category.name}</option>
+            ))}
+        </select>
         <button type="submit" className="btn btn-primary py-1">
           Save
         </button>
@@ -40,6 +79,8 @@ export default function Categories() {
         <thead>
           <tr>
             <td>Category name</td>
+            <td>Parent Category</td>
+            <td></td>
           </tr>
         </thead>
         <tbody>
@@ -47,6 +88,21 @@ export default function Categories() {
             categories.map((category) => (
               <tr>
                 <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className="btn-primary text-sm  mr-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className="btn-primary text-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
