5e2188d7e105d43a643c727e8c4b2dde1b35acd4
demosthenesd <demosthenesd@icloud.com>
2023-08-04 15:44:20 +1000

    10:42:00 || Started working on the All products page | front end

--- client/components/NewProducts.js
+++ client/components/NewProducts.js
@@ -1,11 +1,7 @@
 import styled from "styled-components";
 import Center from "./Center";
-import ProductBox from "./ProductBox";
-const ProductsGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 30px;
-`;
+import ProductsGrid from "./ProductsGrid";
+
 const Title = styled.h2`
   font-size: 2rem;
   margin: 30px 0;
@@ -15,12 +11,7 @@ export default function NewProducts({ products }) {
   return (
     <Center>
       <Title>New Arrivals</Title>
-      <ProductsGrid>
-        {products?.length > 0 &&
-          products.map((product) => (
-            <ProductBox key={product._id} {...product} />
-          ))}
-      </ProductsGrid>
+      <ProductsGrid products={products} />
     </Center>
   );
 }
